f73fcc1a694608286ae84d7d55491d0e25449096
Adrian Borgund <adrian.borgund@scaleaq.com>
2025-02-07 14:51:47 +0100

    Initial setup

---
+++ astro.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "ScaleUI 🐟",
+      logo: {
+        light: "./src/assets/themelight.svg",
+        dark: "./src/assets/themedark.svg",
+        replacesTitle: true,
+      },
+      social: {
+        github: "https://github.com/scaleaq/scaleui-mono",
+        slack: "https://scaleaq.slack.com/archives/C02P7FEQF8E",
+      },
+      customCss: ["./src/styles/custom.css"],
+      sidebar: [
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+    mdx(),
+  ],
+});
